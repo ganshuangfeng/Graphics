c190ddaf9afd122bfb7ef99b07f79f6d4d01a6d1
zhimo1997 <185202204@qq.com>
2019-11-04 02:22:31 +0800

    GPUInstancing简单实例

---
+++ Assets/5-GPUInstancing/Scripts/ColorInstancing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorInstancing : MonoBehaviour
+{
+    GameObject[] objs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objs = GameObject.FindGameObjectsWithTag("GPUInstancing");
+        MaterialPropertyBlock props = new MaterialPropertyBlock();
+        MeshRenderer renderer;
+
+        foreach(GameObject obj in objs)
+        {
+            float r = Random.Range(0.0f,1.0f);
+            float g = Random.Range(0.0f, 1.0f);
+            float b = Random.Range(0.0f, 1.0f);
+            props.SetColor("_Color",new Color(r,g,b));
+
+            renderer = obj.GetComponent<MeshRenderer>();
+            renderer.SetPropertyBlock(props);
+        }
+    }
+}
